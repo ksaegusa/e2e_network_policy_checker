8c9fe8941982578ae4c46e13e234077073c44826
ksaegusa <snack11monster@gmail.com>
2021-11-24 09:09:59 +0900

    add config.ini

--- e2e_network_policy_checker/cli.py
+++ e2e_network_policy_checker/cli.py
@@ -1,12 +1,23 @@
 import os
 import sys
-from csv import reader, writer
 import socket
 import ipaddress
 import concurrent.futures
 
 import click
 import pandas as pd
+
+from logging import basicConfig, getLogger, INFO
+from configparser import ConfigParser
+from csv import reader, writer
+
+config = ConfigParser()
+# TODO: 上書きでファイル指定できるようにする
+config.read(os.path.dirname(os.path.abspath(__file__))+'/config.ini')
+
+# TODO: ログの出力レベルをツール起動時に変更できるようにする
+basicConfig(level=INFO)
+logger = getLogger(__name__)
 
 def send_socket(data):
     """
@@ -16,6 +27,7 @@ def send_socket(data):
     try:
       ipaddress.ip_address(data[0])
     except ValueError:
+        logger.error(f"Input error target_ip={data[0]}")
         return [f"{data[0]}",f"{int(data[1])}",f"Input error target_ip={data[0]}"]
 
     try:
@@ -23,12 +35,14 @@ def send_socket(data):
         if int(data[1]) > 65535:
             raise ValueError
     except ValueError:
+        logger.error(f"Input error port={data[1]}")
         return [f"{data[0]}",f"{data[1]}",f"Input error port={data[1]}"]
 
     # NOTE: TCP-> SOCK_STREAM / UDP -> SOCK_DGRAM
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # TODO: cfgファイルを読み込んで作業者で変更できるようにしたい
-        s.settimeout(1)
+        logger.debug(f"START: {data[0]} {int(data[1])}")
+        logger.debug(f"OPTION: timeout: {int(config['DEFAULT']['socket_timeout'])}")
+        s.settimeout(int(config['DEFAULT']['socket_timeout']))
         return_code = s.connect_ex((data[0], int(data[1])))
         if return_code == 0:
             return [f"{data[0]}",f"{int(data[1])}","OK"]
@@ -36,16 +50,15 @@ def send_socket(data):
             return [f"{data[0]}",f"{int(data[1])}","NG"]
 
 def csv_export(df):
-    # TODO: cfgファイルを読み込んで作業者で変更できるようにしたい
-    _dir = ".tmp"
+    _dir = config['DEFAULT']['save_result_dir']
     if not os.path.exists(_dir):
         os.makedirs(_dir)
-    # TODO: cfgファイルを読み込んで作業者で変更できるようにしたい
-    filename = _dir + "/e2e_network_policy_checker_result.csv"
+    filename = _dir + config['DEFAULT']['result_filename']
     df.to_csv(filename, sep=",",index=False)
     return print(f"Create -> {filename}")
 
 def open_csv(csv):
+    # TODO: csvのフォーマットのバリデーションチェックをする
     with open(csv, 'r') as csv_file:
         csv_reader = reader(csv_file)
         list_of_rows = list(csv_reader)
@@ -60,7 +73,7 @@ def input_user(target_host, ports):
     try:
         ipaddress.ip_address(target_host)
     except ValueError:
-        print(f"Input error: {target_host}")
+        logger.error(f"Input error: {target_host}")
         sys.exit()
 
     if ',' in ports:
@@ -75,10 +88,12 @@ def input_user(target_host, ports):
 @click.option("-t","--target_host", type=str)
 @click.option("-p","--ports", type=str, default='80,8080')
 @click.option("-c","--csv", type=click.Path(exists=True, dir_okay=False))
-def cli(target_host, ports, csv):
+@click.option("-l","--log", type=str, default='INFO')
+def cli(target_host, ports, csv, log):
     """
     
     """
+    logger.debug(f"SCRIPT START")
     if csv:
         data = open_csv(csv)
 
@@ -86,7 +101,8 @@ def cli(target_host, ports, csv):
         data = input_user(target_host, ports)
 
     print("==================================================")
-    with concurrent.futures.ProcessPoolExecutor(max_workers=100) as excuter:
+    logger.debug(f"SCRIPT START")
+    with concurrent.futures.ProcessPoolExecutor(max_workers=int(config['DEFAULT']['process_workers'])) as excuter:
         results = excuter.map(send_socket, data)
         _ip,_port,_msg = list(), list(), list()
         for res in list(results):
